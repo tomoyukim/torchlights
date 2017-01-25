1de8c9a835d1f59a44cb105006e1f9a32781bd6c
tomoyukim <tomoyukim44@gmail.com>
2017-01-25 16:44:38 +0900

    clean up old code

--- index.js
+++ index.js
@@ -19,37 +19,37 @@ io.on('connection', function (socket) {
 
     console.log('id: '+socket.id);
     socket.emit('connected', {
-	userid: socket.id,
-	existedUsers: users
+    userid: socket.id,
+    existedUsers: users
     });
 
     socket.on('login', function(data) {
-	users.push(data);
-	socket.broadcast.emit('login', data);
+    users.push(data);
+    socket.broadcast.emit('login', data);
     });
 
     socket.on('position', function(data) {
-	io.sockets.emit('position', {
-	    userid: socket.id,
-	    position: data
-	});
+    io.sockets.emit('position', {
+        userid: socket.id,
+        position: data
+    });
     });
 
     socket.on('clicked', function(data) {
-	io.sockets.emit('clicked', {
-	    userid: socket.id
-	});
+    io.sockets.emit('clicked', {
+        userid: socket.id
+    });
     });
 
     socket.on('disconnect', function () {
-	users.forEach(function(element, index, array) {
-	    if(element.userid == socket.id){
-		array.splice(index, 1);
-	    }
-	});
-	
-	io.sockets.emit('logout', {
-	    userid: socket.id,
-	});
+    users.forEach(function(element, index, array) {
+        if(element.userid == socket.id){
+        array.splice(index, 1);
+        }
+    });
+
+    io.sockets.emit('logout', {
+        userid: socket.id,
+    });
     });
 });
--- public/sketch.js
+++ public/sketch.js
@@ -5,9 +5,9 @@ var socket = io();
 socket.on('connected', function(data) {
     var users = data.existedUsers;
     for( var i=0; i < users.length; i++){
-	ps.push(new ParticleSystem(users[i].userid, color(users[i].r, users[i].g, users[i].b), users[i].size, true));
+    ps.push(new ParticleSystem(users[i].userid, color(users[i].r, users[i].g, users[i].b), users[i].size, true));
     }
-    
+
     //add myself
     var r = random(100, 255);
     var g = random(100, 255);
@@ -23,26 +23,26 @@ socket.on('login', function(data) {
 
 socket.on('position', function(data) {
     ps.forEach(function(element, index, array){
-	if(element.getId() == data.userid){
-	    element.posX = map(data.position.x, 0, 100, 0, width);
-	    element.posY = map(data.position.y, 0, 100, 0, height);
-	}
+    if(element.getId() == data.userid){
+        element.posX = map(data.position.x, 0, 100, 0, width);
+        element.posY = map(data.position.y, 0, 100, 0, height);
+    }
     });
 });
 
 socket.on('clicked', function(data) {
     ps.forEach(function(element, index, array){
-	if(element.getId() == data.userid){
-	    element.size += 20;
-	}
+    if(element.getId() == data.userid){
+        element.size += 20;
+    }
     });
 });
 
 socket.on('logout', function(data) {
     ps.forEach(function(element, index, array){
-	if(element.getId() == data.userid){
-	    array.splice(index, 1);
-	}
+    if(element.getId() == data.userid){
+        array.splice(index, 1);
+    }
     });
 });
 
@@ -55,12 +55,12 @@ function setup() {
 function draw() {
     background(0);
 
-    socket.emit('position', {x:map(mouseX, 0, width, 0, 100), y:map(mouseY, 0, height, 0, 100)});	
+    socket.emit('position', {x:map(mouseX, 0, width, 0, 100), y:map(mouseY, 0, height, 0, 100)});
 
     if(ps.length > 0){
-	for( var i=0; i < ps.length; i++){
-	    ps[i].run();
-	}
+    for( var i=0; i < ps.length; i++){
+        ps[i].run();
+    }
     }
 }
 
@@ -87,22 +87,22 @@ ParticleSystem.prototype.run = function() {
     this.particles.push(new Particle(this.posX + random(5), this.posY + random(5), this.psColor, this.size, this.size));
 
     for( var i=0; i < this.particles.length; i++){
-	this.particles[i].update();
-	this.particles[i].draw();
+    this.particles[i].update();
+    this.particles[i].draw();
     }
 
     this.particles.forEach(function(element, index, array){
-	if(element.isDead()){
-	    array.splice(index, 1);
-	}
+    if(element.isDead()){
+        array.splice(index, 1);
+    }
     });
 
     if(this.size > this.baseSize){
-	this.size -= 5;
+    this.size -= 5;
     }
 
     if((this.size < this.baseSize)){
-	this.size += 1;
+    this.size += 1;
     }
 }
 
@@ -126,7 +126,7 @@ Particle.prototype.draw = function() {
     fill(this.itsColor, this.life);
     ellipse(this.x, this.y, this.sizeX, this.sizeY);
 }
-  
+
 Particle.prototype.update = function() {
     this.sizeX -= 1;
     this.sizeY -= 1;
